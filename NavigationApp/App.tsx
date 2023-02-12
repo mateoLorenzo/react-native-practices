a33def90a4fc1927b236389df562989f5709c728
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-02-12 19:46:02 -0300

    Installed and configured DRAWER/NAVIGATION

--- NavigationApp/App.tsx
+++ NavigationApp/App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StackNavigator} from './src/navigator/StackNavigator';
+import {DrawerNavigator} from './src/navigator/DrawerNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
--- NavigationApp/src/screens/Page1Screen.tsx
+++ NavigationApp/src/screens/Page1Screen.tsx
@@ -1,29 +1,37 @@
-import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
+import {DrawerScreenProps} from '@react-navigation/drawer';
+import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 
-interface Props extends StackScreenProps<any, any> {}
+interface Props extends DrawerScreenProps<any, any> {}
 
-export const Page1Screen = (props: Props) => {
+export const Page1Screen = ({navigation}: Props) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
+      ),
+    });
+  });
+
   return (
     <View style={styles.globalMargin}>
       <Text style={styles.title}>Page1Screen</Text>
       <Button
         title="Navigate to Page 2"
-        onPress={() => props.navigation.navigate('Page2Screen')}
+        onPress={() => navigation.navigate('Page2Screen')}
       />
       <Text style={styles.page1ScreenSubtitle}>Navigate with arguments</Text>
       <Button
         title="MatuMoto"
         onPress={() =>
-          props.navigation.navigate('PersonScreen', {name: 'MatuMoto', age: 21})
+          navigation.navigate('PersonScreen', {name: 'MatuMoto', age: 21})
         }
       />
       <Button
         title="Gordos Gordos"
         onPress={() =>
-          props.navigation.navigate('PersonScreen', {name: 'El Gordo', age: 15})
+          navigation.navigate('PersonScreen', {name: 'El Gordo', age: 15})
         }
       />
     </View>
