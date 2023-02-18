136bbb26572e00b971dedd78e84433ef5afbec15
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-02-18 17:36:43 -0300

    NavigationApp - Applied LogBox to ignore warning

--- NavigationApp/src/navigator/TopTabNavigator.tsx
+++ NavigationApp/src/navigator/TopTabNavigator.tsx
@@ -3,6 +3,8 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import {ChatScreen} from '../screens/ChatScreen';
 import {ContactsScreen} from '../screens/ContactsScreen';
 import {AlbumsScreen} from '../screens/AlbumsScreen';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Sending']);
 
 const Tab = createMaterialTopTabNavigator();
 
