57d0954a505946cf01ad7937beb8ff8e6694cb9d
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-02-20 17:24:36 -0300

    MoviesApp - Typing response for moviesDBNowPlayng request

--- MoviesApp/src/screens/HomeScreen.tsx
+++ MoviesApp/src/screens/HomeScreen.tsx
@@ -2,13 +2,14 @@ import {StackScreenProps} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlayingResponse} from '../interfaces/movieInterface';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation}: Props) => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(resp => {
-      console.log('response:', resp.data);
+    movieDB.get<MovieDBNowPlayingResponse>('/now_playing').then(resp => {
+      console.log('response:', resp.data.results[0].title);
     });
   }, []);
 
