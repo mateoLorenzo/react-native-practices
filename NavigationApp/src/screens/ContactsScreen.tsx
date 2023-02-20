a6e5a8ec211409b1334ade48ecf88365195287fa
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-02-19 21:17:44 -0300

    NavigationApp/Context - Executing signIn action from ContactsScreen

--- NavigationApp/src/screens/ContactsScreen.tsx
+++ NavigationApp/src/screens/ContactsScreen.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, View} from 'react-native';
 import {Text} from 'react-native-paper';
+import {AuthContext} from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+  const {signIn, authState} = useContext(AuthContext);
+  const {isLoggedIn} = authState;
   return (
     <View>
       <Text>ContactsScreen</Text>
+      {isLoggedIn ? null : <Button title="login" onPress={signIn} />}
     </View>
   );
 };
