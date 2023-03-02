eca55e9189370943727d118220ac595d4fe7fbae
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-03-02 10:51:26 -0300

    MoviesApp - Created useFade custom hook

--- MoviesApp/src/screens/FadeScreen.tsx
+++ MoviesApp/src/screens/FadeScreen.tsx
@@ -1,25 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useRef} from 'react';
+import React from 'react';
 import {Animated, Button, View} from 'react-native';
+import {useFade} from '../hooks/useFade';
 
 export const FadeScreen = () => {
-  const opacity = useRef(new Animated.Value(0)).current;
-
-  const fadeIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
+  const {fadeIn, fadeOut, opacity} = useFade();
 
   return (
     <View
