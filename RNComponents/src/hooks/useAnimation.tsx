915b7f3544a842cef43b59fafb824dab9a5e3aa1
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-03-30 23:17:31 -0300

    RNComponents - Added useAnimation hook with interesting positioning animation

---
+++ RNComponents/src/hooks/useAnimation.tsx
@@ -0,0 +1,49 @@
+import {useRef} from 'react';
+import {Animated} from 'react-native';
+
+export const useAnimation = () => {
+  const opacity = useRef(new Animated.Value(0.4)).current;
+  const position = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const moveBox = (initPosition: number, duration: number = 300) => {
+    position.setValue(initPosition);
+    Animated.timing(position, {
+      toValue: 0,
+      duration,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const resetBox = () => {
+    Animated.timing(position, {
+      toValue: -200,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return {
+    opacity,
+    position,
+    fadeIn,
+    fadeOut,
+    moveBox,
+    resetBox,
+  };
+};
--- RNComponents/src/screens/Animation101Screen.tsx
+++ RNComponents/src/screens/Animation101Screen.tsx
@@ -1,50 +1,22 @@
-import React, {useRef} from 'react';
-import {Animated, Easing, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Animated, Text, TouchableOpacity} from 'react-native';
 import {StyleSheet, View} from 'react-native';
+import {useAnimation} from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
-  const top = useRef(new Animated.Value(-200)).current;
-
-  const fadeIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const centerBox = () => {
-    Animated.timing(top, {
-      toValue: 0,
-      duration: 250,
-      useNativeDriver: true,
-      easing: Easing.bounce,
-    }).start();
-  };
-
-  const resetBox = () => {
-    Animated.timing(top, {
-      toValue: -200,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-  };
+  const {opacity, position, fadeIn, fadeOut, moveBox, resetBox} =
+    useAnimation();
 
   return (
     <View style={styles.container}>
       <Animated.View
-        style={{...styles.purpleBox, opacity, transform: [{translateY: top}]}}
+        style={{
+          ...styles.purpleBox,
+          opacity,
+          transform: [{translateY: position}],
+        }}
       />
-      <TouchableOpacity onPress={centerBox}>
+      <TouchableOpacity onPress={() => moveBox(-1000)}>
         <Text>Center Box</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={resetBox}>
