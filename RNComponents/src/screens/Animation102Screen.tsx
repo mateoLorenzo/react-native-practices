1990514d1b4d277e3ab921f9bab1365ecf28834a
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-03-30 23:51:42 -0300

    RNComponents - Added AnimatedValueXY / Dragable item that returns to center

--- RNComponents/src/screens/Animation102Screen.tsx
+++ RNComponents/src/screens/Animation102Screen.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.purpleBox} />
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.box]}
+      />
     </View>
   );
 };
 const styles = StyleSheet.create({
-  purpleBox: {
-    backgroundColor: 'red',
-    width: 150,
-    height: 150,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: '#61dafb',
+    width: 80,
+    height: 80,
+    borderRadius: 4,
   },
 });
