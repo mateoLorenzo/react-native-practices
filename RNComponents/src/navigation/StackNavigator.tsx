a17d51236ef5249b370ff36f74a521f308195191
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-03-31 23:20:43 -0300

    RNComponents - Switch component

--- RNComponents/src/navigation/StackNavigator.tsx
+++ RNComponents/src/navigation/StackNavigator.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/Home';
 import {Animation101Screen} from '../screens/Animation101Screen';
 import {Animation102Screen} from '../screens/Animation102Screen';
+import {SwitchScreen} from '../screens/SwitchScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export const StackNavigator = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
+      <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
     </Stack.Navigator>
   );
 };
