dc9a36063a3da40c933fd00124c3641640231f43
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-04-03 20:44:54 -0300

    RNComponents - Installed package to show prompt in ios and android

--- RNComponents/src/screens/AlertScreen.tsx
+++ RNComponents/src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Alert, Button, View} from 'react-native';
+import prompt from 'react-native-prompt-android';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
@@ -21,13 +22,34 @@ export const AlertScreen = () => {
   };
 
   const showPrompt = () => {
-    Alert.prompt(
-      'Title',
-      'Message',
-      text => console.log('text:', text),
-      'plain-text',
-      'default text',
-      'number-pad',
+    // Alert.prompt(
+    //   'Title',
+    //   'Message',
+    //   text => console.log('text:', text),
+    //   'plain-text',
+    //   'default text',
+    //   'number-pad',
+    // );
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
     );
   };
 
