b90761f1de298e439eea335e801e8044eca441a9
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-04-05 06:33:36 -0300

    RNComponents - Made adjustes to refresh controll

--- RNComponents/src/screens/PullToRefreshScreen.tsx
+++ RNComponents/src/screens/PullToRefreshScreen.tsx
@@ -1,25 +1,31 @@
 import React, {useState} from 'react';
 import {RefreshControl, ScrollView, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
 export const PullToRefreshScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
+  const {top} = useSafeAreaInsets();
 
   const onRefresh = () => {
     setRefreshing(true);
     setTimeout(() => {
       setRefreshing(false);
+      setData('Hola Mundo');
     }, 1500);
   };
 
   return (
     <ScrollView
+      style={{marginTop: refreshing ? top : 0}}
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }>
       <View style={styles.globalMargin}>
         <HeaderTitle title="Pull To Refresh" />
+        <HeaderTitle title={data ?? ''} />
       </View>
     </ScrollView>
   );
