d55c86d031661aa73f02f2aae9a51c8a8a51ff04
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-04-05 23:57:19 -0300

    RNComponents - Added FadeIn animation to butotn

--- RNComponents/src/navigation/StackNavigator.tsx
+++ RNComponents/src/navigation/StackNavigator.tsx
@@ -18,7 +18,7 @@ const Stack = createStackNavigator();
 export const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
--- RNComponents/src/screens/SlidesScreen.tsx
+++ RNComponents/src/screens/SlidesScreen.tsx
@@ -1,7 +1,18 @@
-import React, {useState} from 'react';
-import {Dimensions, Image, StyleSheet, View} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {
+  Animated,
+  Dimensions,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {ImageSourcePropType, SafeAreaView, Text} from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useAnimation} from '../hooks/useAnimation';
 
 const {width: screenWidth} = Dimensions.get('window');
 
@@ -31,6 +42,17 @@ const items: Slide[] = [
 
 export const SlidesScreen = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const {opacity, fadeIn, fadeOut} = useAnimation();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (activeIndex === items.length - 1) {
+      fadeIn();
+    }
+    if (activeIndex !== 2) {
+      fadeOut();
+    }
+  }, [activeIndex]);
 
   const renderItem = (item: Slide) => (
     <View style={styles.imageContainer}>
@@ -40,6 +62,12 @@ export const SlidesScreen = () => {
     </View>
   );
 
+  const onPress = () => {
+    if (activeIndex === items.length - 1) {
+      navigation.navigate('HomeScreen' as never);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.screenContainer}>
       <Carousel
@@ -48,13 +76,27 @@ export const SlidesScreen = () => {
         sliderWidth={screenWidth}
         itemWidth={screenWidth}
         layout="default"
-        onSnapToItem={index => setActiveIndex(index)}
+        onSnapToItem={index => {
+          setActiveIndex(index);
+        }}
       />
-      <Pagination
-        dotsLength={items.length}
-        activeDotIndex={activeIndex}
-        dotStyle={styles.paginationDot}
-      />
+      <View style={styles.paginationContainer}>
+        <Pagination
+          dotsLength={items.length}
+          activeDotIndex={activeIndex}
+          dotStyle={styles.paginationDot}
+        />
+
+        <Animated.View style={{opacity}}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={onPress}
+            style={styles.enterButton}>
+            <Text style={styles.enterButtonText}>Entrar</Text>
+            <Icon name="chevron-forward-outline" color="white" size={25} />
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -90,5 +132,24 @@ const styles = StyleSheet.create({
     height: 10,
     borderRadius: 10,
     backgroundColor: '#5856D6',
+  },
+  paginationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  enterButton: {
+    flexDirection: 'row',
+    backgroundColor: '#5856D6',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  enterButtonText: {
+    fontSize: 20,
+    color: 'white',
   },
 });
