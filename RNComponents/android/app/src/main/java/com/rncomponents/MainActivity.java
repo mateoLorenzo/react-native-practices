6c6c2365d109d0efbcee1423108f8463727eb255
Mateo Lorenzo <mateolorenzo@Mateos-MacBook-Pro-2.local>
2023-06-19 11:09:04 -0300

    Updating projects to new react native version

--- RNComponents/android/app/src/main/java/com/rncomponents/MainActivity.java
+++ RNComponents/android/app/src/main/java/com/rncomponents/MainActivity.java
@@ -1,4 +1,3 @@
-import android.os.Bundle;
 package com.rncomponents;
 
 import com.facebook.react.ReactActivity;
@@ -7,11 +6,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
