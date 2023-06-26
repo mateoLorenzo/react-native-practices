4cb3400541a092a2ec86528566961ed2c25c61bf
mateolorenzo1 <mateo.lorenzo@flydevs.com>
2023-06-26 09:04:23 -0300

    Pokedex - Initializing interfaces and defining http response type

--- Pokedex/src/hooks/usePokemonPaginated.tsx
+++ Pokedex/src/hooks/usePokemonPaginated.tsx
@@ -1,17 +1,39 @@
-import {useEffect, useRef} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useEffect, useRef, useState} from 'react';
 import {pokemonApi} from '../api/pokemonApi';
+import {
+  PokemonPaginatedResponse,
+  Result,
+  SimplePokemon,
+} from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log('response.data', resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+    nextPageUrl.current = resp.data.next;
+    console.log('resp.data.results', resp.data.results);
+    mapPokemonList(resp.data.results);
+    // setSimplePokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(pokemon => {
+      console.log('pokemon', pokemon);
+    });
   };
 
   useEffect(() => {
     loadPokemons();
   }, []);
 
-  return {};
+  return {
+    simplePokemonList,
+  };
 };
