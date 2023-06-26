d5c4d44fd8f2701c6b08bf16330d160e523de4d6
mateolorenzo1 <mateo.lorenzo@flydevs.com>
2023-06-26 08:49:21 -0300

    Pokedex - Get pokemons list from custom hook

--- Pokedex/src/screens/HomeScreen.tsx
+++ Pokedex/src/screens/HomeScreen.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {Image, Text} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
+  usePokemonPaginated();
+
   return (
     <>
       <Image
