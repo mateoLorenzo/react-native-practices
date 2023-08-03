b3a14f742a3153d226cfb4cc083c5505891d8026
mateolorenzo1 <mateo.lorenzo@flydevs.com>
2023-08-03 08:38:59 -0300

    Pokedex - Created SearchInput component

---
+++ Pokedex/src/components/SearchInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, TextInput, View, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const SearchInput = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textBackground}>
+        <TextInput
+          placeholder="Search Pokemon"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.textInput}
+        />
+        <Icon name="search-outline" color="grey" size={20} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  textBackground: {
+    backgroundColor: '#F3F1F3',
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  textInput: {
+    fontSize: 16,
+    flex: 1,
+    top: Platform.OS === 'ios' ? 0 : 2,
+  },
+});
